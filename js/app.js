f3cd2f39b5a81f5d1e514563e64a8ecc9684dc3f
edwardlea <leaedward87@gmail.com>
2019-01-04 10:19:06 +0000

    Computers boats generated and added to baord

--- js/app.js
+++ js/app.js
@@ -1,55 +1,63 @@
 $(() => {
 
-  // Board Parameters
+  // Board Parameters----------------------------------------------------------
   const boardWidth = 10
   let compHits = 0
   let playerHits = 0
+  let selectedBoat = ''
   const boatsInPlay = []
+  const orientation = ['vertical','horizontial']
   const boats = [
-    {name: 'Carrier',
+    {name: 'carrier',
     length: 5,
     compPlaced: false,
-    playerPlaced: false,
-    compHit: false,
-    playerHit: false},
-    {name: 'Battleship',
+    playerPlaced: true,
+    compHits: 0,
+    playerHits: 0,
+    compSunk: false,
+    playerSunk: false},
+    {name: 'battleship',
     length: 4,
     compPlaced: false,
     playerPlaced: false,
-    compHit: false,
-    playerHit: false},
-    {name: 'Cruiser',
+    compHits: 0,
+    playerHits: 0,
+    compSunk: false,
+    playerSunk: false},
+    {name: 'cruiser',
     length: 3,
     compPlaced: false,
     playerPlaced: false,
-    compHit: false,
-    playerHit: false},
-    {name: 'Carrier',
+    compHits: 0,
+    playerHits: 0,
+    compSunk: false,
+    playerSunk: false},
+    {name: 'submarine',
     length: 3,
     compPlaced: false,
     playerPlaced: false,
-    compHit: false,
-    playerHit: false},
-    {name: 'Carrier',
+    compHits: 0,
+    playerHits: 0,
+    compSunk: false,
+    playerSunk: false},
+    {name: 'destroyer',
     length: 2,
     compPlaced: false,
     playerPlaced: false,
-    compHit: false,
-    playerHit: false},
+    compHits: 0,
+    playerHits: 0,
+    compSunk: false,
+    playerSunk: false},
   ]
 
-  console.log(boats[1].length)
   const boatsArray = ['Carrier', 'Battleship', 'Cruiser', 'Submarine', 'Destroyer']
   const boatLengths = [5, 4, 3, 3, 2]
 
+  // DOM elements-----------------------------------------------------------------
 
   const $playerBoard = $('.playerBoard')
   const $computerBoard = $('.computerBoard')
   const $type = $('.boat-type')
-  const test1 = $type.text()
-  const test = boats['Carrier']
-
-  console.log(test)
 
   // adding board elements to players board
   for(let i = 0; i<boardWidth*boardWidth; i++) {
@@ -57,53 +65,55 @@ $(() => {
   }
   const $grid1 = $playerBoard.find('div')
 
-
   // adding board elements to computers board
   for(let i = 0; i<boardWidth*boardWidth; i++) {
     $computerBoard.append($('<div />'))
-
   }
   const $grid2 = $computerBoard.find('div')
 
   // adding value attribute to each div
 
 
-  // function to add boats to board
+  // Functions--------------------------------------------------------------------
+
+  // function to randomly generate number for placement of ships and selecting hits
+
+  function getRandomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+  }
+
+  // function to add computers boats to board
   function computerBoatPos(){
-    boatsArray.forEach((boat) => {
-
+    boats.forEach((boat) => {
       while (!boat.placed){
-        const boatIndex = generateRandomPosition()
-        const length = boat.length
-        const direction = 'vertical'
+        const boatIndex = getRandomNumber(0, boardWidth*boardWidth)
+        const type = boat.name
+        const direction = orientation[getRandomNumber(0,1)]
         const player = false
-
-        boat.placed = addBoat(boatIndex,length, direction, player)
+        boat.placed = addBoat(boatIndex,type, direction, player)
       }
     })
   }
 
+  computerBoatPos()
+
   // get player clicked position
-  function getBoatInfo(e){
+  function addPlayerBoat(e){
     const boatIndex = $grid1.index($(e.target))
-    const length = boatLengths[1]
+    const type = selectedBoat
+    if(boats.find(boat => boat.name === type).playerPlaced){
+      alert('place again?')
+    }
     const direction = 'vertical'
-    addBoat(boatIndex, length, direction, true)
+    addBoat(boatIndex, type, direction, true)
   }
 
-  // function to randomly generate number for placement of ships and selecting hits
-  function generateRandomPosition(){
-    return Math.floor(Math.random() * (boardWidth * boardWidth))
 
-  }
 
-  function addBoat(boatIndex,length, direction, player){
-    // const boatIndex = $grid1.index($(e.target))
-    console.log(boatIndex)
+  function addBoat(boatIndex, type, direction, player){
     const boatArray = []
-    // const direction = 'vertical'
-    const boatLength = length
-    // creates boar array based on clicked position
+    const boatLength = boats.find(boat => boat.name === type).length
+    // creates boar array based on clicked position and orientation
     if(direction === 'horizontial'){
       for (let i = 0 ; i < boatLength ; i++){
         boatArray.push(boatIndex + i)
@@ -114,21 +124,21 @@ $(() => {
       }
     }
     console.log(boatArray)
+
+    // validation function
+    // selectionValidation (boatArray, player)
     // check boat array vertical condition
     if((boatArray.some(v => v < 0 || v > ((boardWidth * boardWidth)-1)))){
-      // pick another location
       console.log('not valid space 1')
       return false
     }
-
+    // check boat array horizontial condition
     if((boatArray.some(v => v % boardWidth === 0) && (boatArray.some(v => v % boardWidth === (boardWidth - 1))))){
-      // pick another location
       console.log('not valid space 1')
       return false
     }
 
-
-
+    // carried logic relating to boats already applied to the player board
     if(player){
       for (let i = 0 ; i < boatLength ; i++){
         console.log()
@@ -143,6 +153,9 @@ $(() => {
       boatArray.forEach(el => {
         $grid1.eq(el).addClass('boat')
       })
+
+      // boats.find(boat => boat.name === type).length
+      // carried logic relating to boats already applied to the computer board
     } else {
       // check if any element has class of boatArray
       for (let i = 0 ; i < boatLength ; i++){
@@ -159,13 +172,47 @@ $(() => {
         $grid2.eq(el).addClass('boat')
       })
     }
+    return true
   }
 
-  function checkForHit(){
+  // check if players click is located on a boat
+  function checkForHit(e){
+    console.log('checking')
+    const cell = $grid2.index($(e.target))
+    console.log(cell)
+    if($grid2.eq(cell).hasClass('boat')){
+      console.log('boat')
+    }
+    // check if class hit already added
+    // add class of hit to div
+    // increment number of player hits
+
+    checkForWin()
+  }
+
+  // function for computer to pick position on grid and check if match takes place
+  function computerPlay(){
+    // was last go a hit?
+    // add more intellience to guessing
+    // if so next go must be within 1 square
+    // if not random position to be selected
+    // check if class hit already added
+    // add class of hit to div
+    // increment number of player hits
+
+    checkForSink()
+
+    checkForWin()
+  }
+
+  // function to check if new hit has sunk ships
+  // if ship has sunk change class and update array
+  function checkForSink() {
 
   }
 
-  function checkforWin(){
+  // check to see if game has been won. All boats have been hit
+  function checkForWin(){
     if(playerHits === 17) {
       // player wins
     }
@@ -176,15 +223,17 @@ $(() => {
 
 
 
-  function addClass(e){
-    const cell = $grid1.index($(e.target))
-    console.log(cell)
+  function addType(e){
+    selectedBoat = e.target.dataset.boat
+    console.log(selectedBoat)
   }
 
-  
 
 
-  $grid1.on('click', getBoatInfo)
+
+  $grid1.on('click', addPlayerBoat)
+  $grid2.on('click', checkForHit)
+  $type.on('click', addType)
 
 
 
